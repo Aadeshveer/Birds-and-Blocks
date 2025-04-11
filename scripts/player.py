e9572d9482d62d198ae132a33c0af442c1ccc25c
Aadeshveer <24b0926@iitb.ac.in>
2025-04-11 08:46:47 +0000

    Added player 2 functionaity

--- scripts/player.py
+++ scripts/player.py
@@ -5,11 +5,13 @@ class Player:
     '''
     Player class manages the blockmap, cards, birds etc.
     '''
-    def __init__(self, game, origin, block_map = None):
+    def __init__(self, game, identity, origin, block_map = None):
         self.game = game
+        self.identity = identity
         self.origin = origin
+        # initializing tower
         if block_map is not None:
-            self.block_map = BlockMap(origin,block_map)
+            self.block_map = BlockMap(origin, block_map)
         else:
             self.block_map = BlockMap(
                 origin,
@@ -33,11 +35,11 @@ class Player:
                 self.game.display.get_height()
             ),
             (
-                origin[0] + self.block_map.tile_size[0] * 3 + 60,
+                origin[0] + ((self.block_map.tile_size[0] * 3 + 60) if self.identity == 0 else -92),
                 origin[1] - 60
             ),
             ['red', 'red', 'red', 'red', 'red', 'red', 'red'],
-            'left'
+            'left' if self.identity==0 else 'right'
         )
             
 
@@ -46,8 +48,9 @@ class Player:
         Renders all the blocks, projectiles, cards ... 
         '''
         self.block_map.render(self.game.display)
-        if self.deck.active == None:
-            self.game.scrolling = True
-        else:
-            self.game.scrolling = False
-        self.deck.play_deck()
+        if self.game.player_turn == self.identity:
+            if self.deck.active == None:
+                self.game.scrolling = True
+            else:
+                self.game.scrolling = False
+            self.deck.play_deck()
